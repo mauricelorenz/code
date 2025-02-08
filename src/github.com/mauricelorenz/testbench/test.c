3dbc3177d48ef3c3015522f394433c6335439df6
Maurice <maurice.lorenz1@gmx.de>
2025-02-08 12:54:55 +0100

    added a test file in C

---
+++ src/github.com/mauricelorenz/testbench/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+    int x = 5;
+    int *px = &x;
+    printf(
+    "Value (x):    %i\n"
+    "Address (&x): %p\n"
+    "Value (*px):  %i\n"
+    "Address (px): %p\n"
+    , x, &x, *px, px);
+}
+wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
