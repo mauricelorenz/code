7e6dec158c30a43f77ae12770430113d101d0a63
Maurice <maurice.lorenz1@gmx.de>
2025-03-05 19:29:37 +0100

    added test6.c

--- src/github.com/mauricelorenz/testbench/c_programming/test6.c
+++ src/github.com/mauricelorenz/testbench/c_programming/test6.c
@@ -3,5 +3,24 @@
 int main()
 
 {
-    printf("hello world\n");
+    int x = 1;
+    int y = 2;
+
+    printf("%i %i\n", x, y);
+
+    x, y = y, x;
+
+    printf("%i %i\n", x, y);
+
+    /*
+    switch (x)
+    {
+        case 1: printf("1\n");
+        break;
+        case 2: printf("2\n");
+        case 3: printf("3\n");
+        case 4: printf("4\n");
+        case 5: printf("5\n");
+    }
+    */
 }
